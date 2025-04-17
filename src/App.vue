6d8be77f42b280c0cf6551b0b8f01d5ba02b038c
tamarStupp <tamar.stupp@gmail.com>
2025-04-17 21:57:55 +0300

    all except popup

--- src/App.vue
+++ src/App.vue
@@ -1,165 +1,142 @@
 <template id="app">
-	<div class="wrapper">
-		<div class="wrapper">
-			<WheelSvg :hsl="hsl" :colors="['red', 'yellow',]" :colorMode="colorMode" :entriesArray="entriesArray"
-				:rotationAngle="rotationAngle"></WheelSvg>
-			<button id="spin-btn" class="spin-btn" @click="spin" dir="rtl">סובב!</button>
+	<div class="grid">
+		<div class="sidebar">
+			<button v-for="(entry, index) in appData" :class="['category', currCategoryIndex === index ? 'chosenCategory': '']"
+			@click="currCategoryIndex = index"
+			>
+			<div class="black"></div>
+			{{ entry.name }}</button>
 		</div>
-		<button class="edit-wheel" @click="onClickEdit">
-			{{ isEditingMode ? 'סיים': 'ערוך גלגל' }}
-		</button>
-		<div class="edit-screen" v-if="isEditingMode">
-			<div class="input-flex">
-				<div v-for="(exer, index) in entriesArray" :key="'exer' + index" class="input-and-btn">
-					<button class="minus" @click="entriesArray.splice(index, 1);">
-						<div class="minus-icon">-</div>
-					</button>
-					<input v-model="entriesArray[index]" type="text" class="exer-input"/>
-				</div>
-				<button @click="entriesArray.push('')" class="plus">+</button>
-			</div>
-		</div>
-
+		<Wheel :entriesArray="appData[currCategoryIndex].items" @save-to-local="saveToLocal"></Wheel>
 	</div>
 </template>
 
 
 <script>
-import WheelSvg from "./components/WheelSvg.vue"
+import Wheel from "./components/Wheel.vue"
 
 export default {
-	components: { WheelSvg },
+	components: { Wheel },
 	data() {
 		return {
-			colorMode: 'hsl',
-			entriesArray: ['אימון 1', "שלום לכולם היום נראה משהו מיוחד", "אימון 3", "אימון 5", "אימון 6"],
-			hsl: {
-				startHue: 20,
-				hueJump: 23
-			},
-			rotationAngle: 0,
-			isEditingMode: false
+			currCategoryIndex: 0,
+			appData: [
+				{
+					name: "כתפיים",
+					items: [""]
+				},
+				{
+					name: "חזה",
+					items: [""]
+				},
+				{
+					name: "יד קדמית",
+					items: ["" ]
+				},
+				{
+					name: "יד אחורית",
+					items: [""]
+				},
+				{
+					name: "אצבעות",
+					items: [""]
+				},
+				{
+					name: "בטן",
+					items: [""]
+				},
+				{
+					name: "גב",
+					items: [""]
+				},
+				{
+					name: "המסטרינג",
+					items: [""]
+				},
+				{
+					name: "ארבע ראשי",
+					items: [""]
+				},
+				{
+					name: "תאומים וקרסוליים",
+					items: [""]
+				},
+			]
+		}
+	},
+	created() {
+		if (localStorage.getItem('appData')) {
+			this.appData = JSON.parse(localStorage.getItem('appData'));
 		}
 	},
 	methods: {
-		onSwitch() {
-			if (this.colorMode === "hsl") {
-				this.colorMode = "array";
-			} else {
-				this.colorMode = "hsl";
-			}
-		},
-		onClickEdit() {
-			if (this.isEditingMode === true) {
-				localStorage.setItem('entriesArray', JSON.stringify(this.entriesArray));
-			}
-			this.isEditingMode = !this.isEditingMode;
-		},
-		spin() {
-			const randomInt = Math.floor(Math.random() * this.entriesArray.length);
-			const randomFullSpins = Math.floor(Math.random() * (4 - 2) + 1);
-			this.rotationAngle += 2 * Math.PI * randomFullSpins;
-			const radians = this.rotationAngle % (2*Math.PI);
-			this.rotationAngle += randomInt * this.radianPerSlice;
-			if ((this.rotationAngle - Math.PI/2 - this.radianPerSlice/2) % (this.radianPerSlice) !== 0) {
-				console.log('hi');
-				this.rotationAngle += ((this.radianPerSlice) - (this.rotationAngle - Math.PI/2 - this.radianPerSlice/2) % (this.radianPerSlice));
-			}
+		saveToLocal() {
+			localStorage.setItem('appData', JSON.stringify(this.appData));
 		}
-	},
-	computed: {
-		radianPerSlice() {
-			return (2 * Math.PI / this.entriesArray.length)
-		},
-	},
-	created() {
-		this.entriesArray = JSON.parse(localStorage.getItem('entriesArray'));
-		this.rotationAngle = Math.PI / 2 - this.radianPerSlice / 2;
+
 	}
 }
 
 </script>
 
 <style scoped>
-button {
-	cursor: pointer;
-}
+	.grid {
+		display: grid;
+		grid-template-columns: 25fr 75fr;
+		width: 100%;
+		height: 100%;
+	}
 
-.wrapper {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	/* height: 100%; */
-	padding-top: 1rem;
-}
+	.category {
+		cursor: pointer;
+		background-color: transparent;
+		outline: none;
+		border: none;
+		border-bottom: 1px solid black;
+		position: relative;
+		transition: all 0.5s;
+		font-size: 1.2rem;
+	}
 
-.spin-btn {
-	background-color: pink;
-	border: solid black 1px;
-	margin-inline: 1rem;
-	padding: 0.25rem 0.5rem;
-	aspect-ratio: 1/1;
-	height: 48px;
-	border-radius: 2rem 2rem;
-	cursor: pointer;
-	margin-top: 1rem;
-}
+	.category .black {
+		display: block;
+		position: absolute;
+		bottom: -10%;
+		left: 0%;
+		z-index: -1;
+		background-color: rgb(54, 54, 54);
+		width: 100%;
+		/* transition: height 3s; */
+	}
 
-.hsl-btns {
-	display: contents;
-}
+	.sidebar {
+		height: 100%;
+		display: inline-flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		padding-block: 3rem;
+		padding-right: 0.5rem;
+		box-sizing: border-box;
+		gap: 48px;
+		overflow-y: auto;
+		overflow-x: hidden;
+	}
 
-.edit-wheel {
-	background-color: pink;
-	border: solid black 1px;
-	margin-inline: 1rem;
-	padding: 0.25rem 0.5rem;
-	min-height: 48px;
-	border-radius: 0.2rem 0.2rem;
-	cursor: pointer;
-	margin-top: 1rem;
-}
+	.chosenCategory {
+		color: rgb(243, 243, 243)
+	}
 
-.input-flex {
-	max-height: 35vh;
-	overflow-y: auto;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	gap: 12px;
-}
+	.chosenCategory .black {
+		animation: expand 0.3s forwards;
+	}
 
-.exer-input {
-	min-height: 1.5rem;
-}
+	@keyframes expand {
+		from {
+			height: 0%;
+		} to {
+			height: 120%;
+		}
+	}
 
-.minus {
-    /* margin-inline: max(1rem, 36px); */
-    height: 48px;
-	border: none;
-	background-color: transparent;
-}
-
-.minus-icon {
-	box-sizing: border-box;
-	background-color: pink;
-    border: solid black 1px;
-	padding: 0.25rem 0.6rem;
-	border-radius: 2rem 2rem;
-	aspect-ratio: 1 / 1;
-	cursor: pointer;
-}
-
-.plus {
-	box-sizing: border-box;
-	background-color: pink;
-    border: solid black 1px;
-	padding: 0.25rem 0.6rem;
-	border-radius: 2rem 2rem;
-	aspect-ratio: 1 / 1;
-	cursor: pointer;
-	align-self: center;
-}
 </style>
